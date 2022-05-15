526828a2e918b693da1656f3c481f925b10d012b
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-05-15 20:10:08 +0900

    [REMOVE] 기획에서 삭제된 event 에 대한 type constant 제거

--- src/main/kotlin/com/iplease/lib/bento/impl/event/type/IpDemandEventTypeV1.kt
+++ src/main/kotlin/com/iplease/lib/bento/impl/event/type/IpDemandEventTypeV1.kt
@@ -2,21 +2,24 @@ package com.iplease.lib.bento.impl.event.type
 
 import com.iplease.lib.bento.api.event.data.EventData
 import com.iplease.lib.bento.api.event.type.EventType
+import com.iplease.lib.bento.impl.event.data.ip.demand.*
+import com.iplease.lib.bento.impl.event.data.ip.demand.info.IpDemandInfoUpdateEvent
+import com.iplease.lib.bento.impl.event.data.ip.demand.status.IpDemandStatusAcceptEvent
+import com.iplease.lib.bento.impl.event.data.ip.demand.status.IpDemandStatusConfirmEvent
+import com.iplease.lib.bento.impl.event.data.ip.demand.status.IpDemandStatusRejectEvent
 import kotlin.reflect.KClass
 
 enum class IpDemandEventTypeV1(
     private val routingKey: String,
     private val eventDataType: KClass<out EventData>
 ): EventType {
-    IP_DEMAND_INFO_UPDATE("v1.event.ip.demand.info.update", EventData::class),
-    IP_DEMAND_ADD("v1.event.ip.demand.add", EventData::class),
-    IP_DEMAND_CREATE("v1.event.ip.demand.create", EventData::class),
-    IP_DEMAND_CANCEL("v1.event.ip.demand.cancel", EventData::class),
-    IP_DEMAND_STATUS_CONFIRM("v1.event.ip.demand.status.confirm", EventData::class),
-    IP_DEMAND_STATUS_ACCEPT("v1.event.ip.demand.status.accept", EventData::class),
-    IP_DEMAND_STATUS_REJECT("v1.event.ip.demand.status.reject", EventData::class),
-    IP_DEMAND_SUCCESS("v1.event.ip.demand.success", EventData::class),
-    IP_DEMAND_FAILED("v1.event.ip.demand.failed", EventData::class);
+    IP_DEMAND_INFO_UPDATE("v1.event.ip.demand.info.update", IpDemandInfoUpdateEvent::class),
+    IP_DEMAND_CREATE("v1.event.ip.demand.create", IpDemandCreateEvent::class),
+    IP_DEMAND_STATUS_CONFIRM("v1.event.ip.demand.status.confirm", IpDemandStatusConfirmEvent::class),
+    IP_DEMAND_STATUS_ACCEPT("v1.event.ip.demand.status.accept", IpDemandStatusAcceptEvent::class),
+    IP_DEMAND_STATUS_REJECT("v1.event.ip.demand.status.reject", IpDemandStatusRejectEvent::class),
+    IP_DEMAND_SUCCESS("v1.event.ip.demand.success", IpDemandSuccessEvent::class),
+    IP_DEMAND_FAILED("v1.event.ip.demand.failed", IpDemandFailedEvent::class);
 
     override fun getRoutingKey() = routingKey
     override fun getEventDataType() = eventDataType
