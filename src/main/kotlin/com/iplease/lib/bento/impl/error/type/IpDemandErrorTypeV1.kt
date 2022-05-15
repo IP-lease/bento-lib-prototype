b209311b9f7f067773904ae4376204756eb00c37
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-05-16 08:38:46 +0900

    [FIX] ErrorType 스펙 변경

--- src/main/kotlin/com/iplease/lib/bento/impl/error/type/IpDemandErrorTypeV1.kt
+++ src/main/kotlin/com/iplease/lib/bento/impl/error/type/IpDemandErrorTypeV1.kt
@@ -2,21 +2,26 @@ package com.iplease.lib.bento.impl.error.type
 
 import com.iplease.lib.bento.api.error.data.ErrorData
 import com.iplease.lib.bento.api.error.type.ErrorType
+import com.iplease.lib.bento.impl.error.data.ip.demand.IpDemandCreateError
+import com.iplease.lib.bento.impl.error.data.ip.demand.IpDemandFailedError
+import com.iplease.lib.bento.impl.error.data.ip.demand.IpDemandSuccessError
+import com.iplease.lib.bento.impl.error.data.ip.demand.info.IpDemandInfoUpdateError
+import com.iplease.lib.bento.impl.error.data.ip.demand.status.IpDemandStatusAcceptError
+import com.iplease.lib.bento.impl.error.data.ip.demand.status.IpDemandStatusConfirmError
+import com.iplease.lib.bento.impl.error.data.ip.demand.status.IpDemandStatusRejectError
 import kotlin.reflect.KClass
 
 enum class IpDemandErrorTypeV1(
     private val routingKey: String,
     private val eventDataType: KClass<out ErrorData>
 ): ErrorType {
-    IP_DEMAND_INFO_UPDATE("v1.error.ip.demand.info.update", ErrorData::class),
-    IP_DEMAND_ADD("v1.error.ip.demand.add", ErrorData::class),
-    IP_DEMAND_CREATE("v1.error.ip.demand.create", ErrorData::class),
-    IP_DEMAND_CANCEL("v1.error.ip.demand.cancel", ErrorData::class),
-    IP_DEMAND_STATUS_CONFIRM("v1.error.ip.demand.status.confirm", ErrorData::class),
-    IP_DEMAND_STATUS_ACCEPT("v1.error.ip.demand.status.accept", ErrorData::class),
-    IP_DEMAND_STATUS_REJECT("v1.error.ip.demand.status.reject", ErrorData::class),
-    IP_DEMAND_SUCCESS("v1.error.ip.demand.success", ErrorData::class),
-    IP_DEMAND_FAILED("v1.error.ip.demand.failed", ErrorData::class);
+    IP_DEMAND_INFO_UPDATE("v1.error.ip.demand.info.update", IpDemandInfoUpdateError::class),
+    IP_DEMAND_CREATE("v1.error.ip.demand.create", IpDemandCreateError::class),
+    IP_DEMAND_STATUS_CONFIRM("v1.error.ip.demand.status.confirm", IpDemandStatusConfirmError::class),
+    IP_DEMAND_STATUS_ACCEPT("v1.error.ip.demand.status.accept", IpDemandStatusAcceptError::class),
+    IP_DEMAND_STATUS_REJECT("v1.error.ip.demand.status.reject", IpDemandStatusRejectError::class),
+    IP_DEMAND_SUCCESS("v1.error.ip.demand.success", IpDemandSuccessError::class),
+    IP_DEMAND_FAILED("v1.error.ip.demand.failed", IpDemandFailedError::class);
 
     override fun getRoutingKey() = routingKey
     override fun getErrorDataType() = eventDataType
--- src/main/kotlin/com/iplease/lib/bento/impl/error/type/IpReleaseErrorTypeV1.kt
+++ src/main/kotlin/com/iplease/lib/bento/impl/error/type/IpReleaseErrorTypeV1.kt
@@ -2,17 +2,14 @@ package com.iplease.lib.bento.impl.error.type
 
 import com.iplease.lib.bento.api.error.data.ErrorData
 import com.iplease.lib.bento.api.error.type.ErrorType
+import com.iplease.lib.bento.impl.error.data.ip.release.IpReleaseSuccessError
 import kotlin.reflect.KClass
 
 enum class IpReleaseErrorTypeV1(
     private val routingKey: String,
-    private val eventDataType: KClass<ErrorData>
+    private val eventDataType: KClass<out ErrorData>
 ): ErrorType {
-    IP_RELEASE_OFFER_ADD("v1.error.ip.release.offer.add", ErrorData::class),
-    IP_RELEASE_OFFER_CANCEL("v1.error.ip.release.offer.cancel", ErrorData::class),
-    IP_RELEASE_OFFER_ACCEPT("v1.error.ip.release.offer.accept", ErrorData::class),
-    IP_RELEASE_RESERVE_ADD("v1.error.ip.release.reserve.add", ErrorData::class),
-    IP_RELEASE_RESERVE_CANCEL("v1.error.ip.release.reserve.cancel", ErrorData::class);
+    IP_RELEASE_SUCCESS("v1.error.ip.release.released", IpReleaseSuccessError::class);
 
     override fun getRoutingKey() = routingKey
     override fun getErrorDataType() = eventDataType
