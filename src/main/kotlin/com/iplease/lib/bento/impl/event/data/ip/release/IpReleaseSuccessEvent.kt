d61fe8b296c43d86bba83e523a3cbab7b6ef55db
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-05-15 20:31:43 +0900

    [FIX] 누락된 코드 및 자잘한 버그 수정

--- src/main/kotlin/com/iplease/lib/bento/impl/event/data/ip/release/IpReleaseSuccessEvent.kt
+++ src/main/kotlin/com/iplease/lib/bento/impl/event/data/ip/release/IpReleaseSuccessEvent.kt
@@ -1,7 +1,9 @@
 package com.iplease.lib.bento.impl.event.data.ip.release
 
+import com.iplease.lib.bento.api.event.data.EventData
+
 data class IpReleaseSuccessEvent (
     val assignedIpUuid: Long,
     val demandUuid: Long,
     val operatorUuid: Long
-)
+): EventData
--- src/main/kotlin/com/iplease/lib/bento/impl/event/type/IpReleaseEventTypeV1.kt
+++ src/main/kotlin/com/iplease/lib/bento/impl/event/type/IpReleaseEventTypeV1.kt
@@ -2,13 +2,14 @@ package com.iplease.lib.bento.impl.event.type
 
 import com.iplease.lib.bento.api.event.data.EventData
 import com.iplease.lib.bento.api.event.type.EventType
+import com.iplease.lib.bento.impl.event.data.ip.release.IpReleaseSuccessEvent
 import kotlin.reflect.KClass
 
 enum class IpReleaseEventTypeV1(
     private val routingKey: String,
-    private val eventDataType: KClass<EventData>
+    private val eventDataType: KClass<out EventData>
 ): EventType {
-    IP_RELEASE_SUCCESS("v1.event.ip.release.released", EventData::class);
+    IP_RELEASE_SUCCESS("v1.event.ip.release.released", IpReleaseSuccessEvent::class);
 
     override fun getRoutingKey() = routingKey
     override fun getEventDataType() = eventDataType
