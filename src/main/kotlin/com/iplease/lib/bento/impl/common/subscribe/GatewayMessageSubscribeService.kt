33d4d8058678e20ee93c80b36b0ff0b3f671344a
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-05-16 13:10:41 +0900

    [FIX] 네이밍 변경

--- src/main/kotlin/com/iplease/lib/bento/impl/common/subscribe/GatewayMessageSubscribeService.kt
+++ src/main/kotlin/com/iplease/lib/bento/impl/common/subscribe/GatewayMessageSubscribeService.kt
@@ -4,7 +4,7 @@ import com.iplease.lib.bento.api.common.listener.MessageListener
 import com.iplease.lib.bento.api.common.message.Message
 import com.iplease.lib.bento.api.common.subscribe.MessageSubscribeService
 
-abstract class RouteMessageSubscribeService: MessageSubscribeService {
+abstract class GatewayMessageSubscribeService: MessageSubscribeService {
     final override fun subscribe(message: Message) { getListeners().forEach { it.subscribe(message) } }
     abstract fun getListeners(): Iterable<MessageListener>
 }
