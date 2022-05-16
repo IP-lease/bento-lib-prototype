a8db1c163d974bf88e8a8470433792c3b469b670
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-05-16 10:38:31 +0900

    [FIX] RouteMessageSubscribeService 를 api 에서 impl 패키지로 이전

--- src/main/kotlin/com/iplease/lib/bento/impl/global/subscribe/RouteMessageSubscribeService.kt
+++ src/main/kotlin/com/iplease/lib/bento/impl/global/subscribe/RouteMessageSubscribeService.kt
@@ -1,7 +1,8 @@
-package com.iplease.lib.bento.api.global.subscribe
+package com.iplease.lib.bento.impl.global.subscribe
 
 import com.iplease.lib.bento.api.global.listener.MessageListener
 import com.iplease.lib.bento.api.global.message.Message
+import com.iplease.lib.bento.api.global.subscribe.MessageSubscribeService
 
 abstract class RouteMessageSubscribeService: MessageSubscribeService {
     final override fun subscribe(message: Message) { getListeners().forEach { it.subscribe(message) } }
